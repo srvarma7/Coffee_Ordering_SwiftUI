2afe85f7fe6ba7ecb1c1e41ebaf6af1d2d814d51
srvarma7 <srvarma7@gmail.com>
2020-09-03 15:01:41 +1000

    Adding project files

---
+++ CoffeeOrdering/CoffeeOrdering/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeOrdering
+//
+//  Created by Sai Raghu Varma Kallepalli on 3/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
